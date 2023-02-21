9af55c2e3b0064fc275713f7cf8dbe88102ff446
vasco sousa <vasco.alex.sousa@gmail.com>
2023-02-21 15:43:07 +0000

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   post '/tasks', to: 'tasks#create'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch '/tasks/:id', to: 'tasks#update'
   delete '/tasks/:id', to: 'tasks#destroy'
   patch 'tasks/:id', to: 'tasks#update', as: 'update_task'
 end
